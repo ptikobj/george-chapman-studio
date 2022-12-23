d40c4be0b8f1a24a1ab7850b34c7807bcbdbf953
George Chapman <george@gnchapman.com>
2022-12-23 10:49:17 +0000

    fixed tracking code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -115,6 +115,10 @@ window.addEventListener('load', function () {
 // matomo
 var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+_paq.push(["setCookieDomain", "*.gnchapman.com"]);
+_paq.push(["setDomains", ["*.gnchapman.com","*.georgechapman.studio"]]);
+_paq.push(["enableCrossDomainLinking"]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
@@ -122,5 +126,5 @@ _paq.push(['enableLinkTracking']);
   _paq.push(['setTrackerUrl', u+'matomo.php']);
   _paq.push(['setSiteId', '1']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
--- assets/js/matomo.js
+++ assets/js/matomo.js
@@ -1,5 +1,9 @@
   var _paq = window._paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setCookieDomain", "*.gnchapman.com"]);
+  _paq.push(["setDomains", ["*.gnchapman.com","*.georgechapman.studio"]]);
+  _paq.push(["enableCrossDomainLinking"]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
@@ -7,5 +11,5 @@
     _paq.push(['setTrackerUrl', u+'matomo.php']);
     _paq.push(['setSiteId', '1']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
