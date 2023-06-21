bd9f8b203ed4a76cab9434551cd9380f88091fdf
George Chapman <george@gnchapman.com>
2023-06-21 12:00:23 +0100

    created dev branch

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -96,19 +96,3 @@ window.addEventListener('load', function () {
   underlineCurrentPage();
 
 });
-
-var _paq = window._paq = window._paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-_paq.push(["setCookieDomain", "*.georgechapman.studio"]);
-_paq.push(["setDomains", ["*.georgechapman.studio","*.gnchapman.com"]]);
-_paq.push(["enableCrossDomainLinking"]);
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-  var u="https://matomo.gnchapman.com/";
-  _paq.push(['setTrackerUrl', u+'matomo.php']);
-  _paq.push(['setSiteId', '1']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-})();
